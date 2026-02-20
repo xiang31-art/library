73d6d709f80ba24e6c240749e89df4587d76a3ce
TAKOYAKI5733 <hosoyachiharuka@gmail.com>
2026-02-20 16:43:53 +0900

    add

--- hosoyachi/prac.cpp
+++ hosoyachi/prac.cpp
@@ -36,12 +36,32 @@ public:
   // 本の貸出
   void borrow_book(int input_num)
   {
+    if (input_num > books.size() - 1 || input_num < 0)
+    {
+      cout << "予期せぬ値が入力されました" << endl;
+      return;
+    }
     if (books.at(input_num).avail == 0)
     {
       cout << "その本の在庫はありません" << endl;
       return;
     }
+
     books.at(input_num).avail--;
+    cout << books.at(input_num).title << "を借りました。返却期限は2週間です" << endl;
+  }
+
+  // 本の返却
+  void return_book(int input_num)
+  {
+    if (input_num > books.size() - 1 || input_num < 0)
+    {
+      cout << "予期せぬ値が入力されました" << endl;
+      return;
+    }
+
+    books.at(input_num).avail++;
+    cout << books.at(input_num).title << "を返却しました。" << endl;
   }
 };
 
@@ -62,6 +82,8 @@ int main()
       string title, author;
       int year, avail;
       double price;
+
+      cout << "\n";
       cout << "本の情報を入力してください(著名):";
       cin >> title;
       cout << "本の情報を入力してください(著者):";
@@ -72,6 +94,9 @@ int main()
       cin >> price;
       cout << "本の情報を入力してください(在庫数):";
       cin >> avail;
+      cout << "\n";
+      cout << "本の情報を追加しました" << endl;
+      cout << "\n";
 
       BM.add_book(title, author, year, price, avail);
       break;
@@ -79,6 +104,7 @@ int main()
     case 2:
     {
       cout << "\n";
+      cout << "本の情報一覧" << endl;
       for (const Book &b : BM.books_view())
       {
         cout << "タイトル:" << b.title << " 著者:" << b.author << " 出版年:" << b.year << " 価格:" << b.price << " 在庫:" << ((b.avail != 0) ? "あり" : "なし") << endl;
@@ -91,6 +117,8 @@ int main()
     {
       int input_num;
       vector<Book> books = BM.books_view();
+
+      cout << "\n";
       for (int i = 0; i < books.size(); i++)
       {
         cout << i << " タイトル:" << books.at(i).title << " 在庫数:" << books.at(i).avail << endl;
@@ -98,11 +126,29 @@ int main()
       cout << "借りたい本の番号を入力:";
       cin >> input_num;
       BM.borrow_book(input_num);
+
+      cout << "\n";
+
       break;
     }
 
     case 4:
     {
+      int input_num;
+      vector<Book> books = BM.books_view();
+
+      cout << "\n";
+      for (int i = 0; i < books.size(); i++)
+      {
+        cout << i << " タイトル:" << books.at(i).title << " 在庫数:" << books.at(i).avail << endl;
+      }
+      cout << "返したい本の番号を入力:";
+      cin >> input_num;
+      BM.return_book(input_num);
+
+      cout << "\n";
+
+      break;
     }
 
     case 5:
